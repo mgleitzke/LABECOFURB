27736c127defb38920fbced35b573ef1214ad9ea
mgleitzke <mateusleitzke2014@gmail.com>
2025-03-24 21:31:54 -0300

    Add files via upload

---
+++ Aula5.R
@@ -0,0 +1,26 @@
+# API (porta de acesso remoto)
+# acessamos dados do bano mundia (world bank)
+# world develompmente indicators (wdi)
+# indicadores de desenvolvimento mundial
+
+# PIB (PRODUTO INTERNO BRUTO)
+
+#library(wdi) #carregar biblioteca/pacote
+
+#procurar dados no word bank
+
+# install.packages('WDI')
+
+#library(WDI) # SEMPRE CHAMAR OS PACOTE
+
+#recursosnaturaispainel <- WDI(country = 'all', 
+                                   #indicator = 'NY.ADJ.DRES.GN.ZS')
+
+#recursosnaturaistransversal <- WDI(country = 'all',
+                                   #indicator = 'NY.ADJ.DRES.GN.ZS',
+                                   #start = 2020,end = 2020)
+                                   
+#recursosnaturaistemporal <- WDI(country = 'BR',
+                                #indicator = 'NY.ADJ.DRES.GN.ZS')
+                    
+
