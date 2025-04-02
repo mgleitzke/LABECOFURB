39443f51e97a52bf9e40ef8c4eb6bee7c29380a6
mgleitzke <mateusleitzke2014@gmail.com>
2025-04-01 21:17:58 -0300

    Update Dados (em tabela) indicator recursos naturais.R

--- Dados (em tabela) indicator recursos naturais.R
+++ Dados (em tabela) indicator recursos naturais.R
@@ -6,7 +6,7 @@ recnatpainel <- WDI(country = 'all',
 
 recnattransversal <- WDI(country = 'all',
                          indicator = 'NY.ADJ.DRES.GN.ZS',
-                         start = 2022,end = 2022)
+                         start = 2021,end = 2021)
 
 recnattemporal <- WDI(country = 'BR',
                       indicator = 'NY.ADJ.DRES.GN.ZS')
